3b86c8c0f8e64519b48f57e75a81be41e6f5973e
newbie <594424877@qq.com>
2014-06-14 22:20:21 +0800

    completely use ET for XML

--- youdao.py
+++ youdao.py
@@ -49,65 +49,46 @@ UNDERLINE = "\033[4m";
 NORMAL = "\033[m";
 RED = "\033[1;31m"
 def crawl_xml(queryword):
-	return urllib2.urlopen("http://dict.yodao.com/search?keyfrom=dict.python&q="
-        + urllib.quote_plus(queryword) + "&xmlDetail=true&doctype=xml").read();
+	return urllib2.urlopen("http://dict.yodao.com/search?q="
+        + urllib.quote_plus(queryword) + "&doctype=xml").read();
 def print_translations(xml, with_color, detailed):
 
-	#original_query = get_elements(xml, "original-query");
 	root = ET.fromstring(xml);
 	original_query = root.find("original-query");
-	#queryword = get_text(original_query[0]);
 	queryword = original_query.text;
 
-	#custom_translations = get_elements(xml, "custom-translation");
 	custom_translations = root.findall("custom-translation");
 	print BOLD + UNDERLINE + queryword + NORMAL;
 	translated = False;
 	
 	for cus in custom_translations:
-		#source = get_elements_by_path(cus, "source/name");
 		source = cus.find("source/name");
-		
-		#print RED + "Translations from " + source[0] + DEFAULT;
 		print RED + "Translations from " + source.text + DEFAULT;
-		#contents = get_elements_by_path(cus, "translation/content");
 		contents = cus.findall("translation/content");
 		if with_color:
 			for content in contents[0:5]:
-				#print GREEN + get_text(content) + DEFAULT;
 				print GREEN + content.text + DEFAULT;
 		else:
 			for content in contents[0:5]:
-				#print get_text(content);
 				print content.text;
 		translated = True;
 
-	yodao_translations = get_elements(xml, "yodao-web-dict");
+	yodao_translations = root.findall("yodao-web-dict");
 	printed = False;
 	for trans in yodao_translations:
-                webtrans = get_elements(trans, "web-translation");
+                webtrans = trans.findall("web-translation");
 		for web in webtrans[0:5]:
 			if not printed:
 				print RED + "Translations from yodao:" + DEFAULT;
 				printed = True;
-	        	keys = get_elements(web, "key");
-			values = get_elements_by_path(web, "trans/value");
-			summaries = get_elements_by_path(web, "trans/summary");
-			key = keys[0].strip();
-			value = values[0].strip();
-			#summary = summaries[0].strip();
-                        #lines = get_elements(summary, "line");
-		        if with_color:
-			       	print BOLD +  get_text(key) + ":\t" +DEFAULT + GREEN + get_text(value) + NORMAL;
-                                #for line in lines:
-                                #        print GREEN + get_text(line) + DEFAULT;
-				#print get_text(summary) + DEFAULT;
-		        else:
-				print get_text(value);
-				#print get_text(summary);
-		        #translated = True;
-		        #if not detailed:
-			#        break
+	        	key = web.find("key");
+			values = web.findall("trans/value");
+			key = key.text.strip();
+			value = values[0].text.strip();
+			if with_color:
+				print BOLD +  key + ":\t" + DEFAULT + GREEN + value + NORMAL;
+			else:
+				print value;
 	
 def usage():
 	print "usage: dict.py word_to_translate";
